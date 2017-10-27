9f58e388a7650870d54f98d962de7f0a380c3602
Erik Liubakka <erik.liubakka97@gmail.com>
2017-10-27 12:41:24 -0400

    Add Makefile, refactor calculate function.

--- rpn.py
+++ rpn.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
+import operator
+
+ops = {
+	'+': operator.add,
+	'-': operator.sub,
+}
+
 def calculate(equation):
 	stack = list()
 	for arg in equation.split():
-		if arg == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		else:
+		try:
 			stack.append(int(arg))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[arg]
+			result = function(arg1, arg2)
+			stack.append(result)
 	print(stack)
 	return stack.pop()
 
